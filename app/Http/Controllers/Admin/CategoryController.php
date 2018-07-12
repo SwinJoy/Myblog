08b052c69313855ded2a1bb71cc614d548ec7aa5
SwinJoy <1066684692@qq.com>
2018-07-12 16:17:05 +0800

    后台分类页多级分类列表

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -12,9 +12,33 @@ class CategoryController extends CommonController
     //get.admin/category  全部分类列表
     public function index()
     {
-        $categorys = Category::all();
+        $categorys = (new Category)->tree();
+//        dd($categorys);
+//        $data = $this->getTree($categorys,'cate_name','cate_id','cate_pid');
+
         return view('admin.category.index')->with('data',$categorys);
     }
+
+    //得到分类下的子分类   先通过foreach遍历category表下的$data数据，通过值$v下的cate_pid字段等于0分出大分类，把键$data[$k]放进$arr数组中
+    //再通过foreach遍历遍历category表下的$data数据，通过值$n下的cate_pid字段等于大分类的值$v下的cate_id字段分出子分类，把子分类排到大分类后面
+//    public function getTree($data)
+//    {
+//        $arr = array();
+//       foreach ($data as $k=>$v){
+//           if ($v->cate_pid==0){
+//               $data[$k]['_cate_name'] = $data[$k]['cate_name'];
+//               $arr[] = $data[$k];
+//               foreach ($data as $m=>$n){
+//                   if ($n->cate_pid == $v->cate_id){
+//                       $data[$m]['_cate_name'] = "┣━ ".$data[$m]['cate_name'];
+//                       $arr[] = $data[$m];
+//                   }
+//               }
+//           }
+//       }
+//      return $arr;
+//    }
+
 
     //post.admin/category
     public function store()
--- app/Http/Model/Category.php
+++ app/Http/Model/Category.php
@@ -10,4 +10,31 @@ class Category extends Model
     protected $table = 'category';//表名
     protected $primaryKey = 'cate_id';//主键
     public $timestamps = false;//把update_at更新禁用
+
+
+    public function tree()
+    {
+        $categorys = $this->all();
+        return $this->getTree($categorys,'cate_name','cate_id','cate_pid');
+    }
+
+
+    //分类下的子分类排序   getTree(数组参数,name字段,id字段,父级id字段,默认顶级父级id)
+    public function getTree($data,$file_name,$file_id='id',$file_pid='pid',$pid='0')
+    {
+        $arr = array();
+        foreach ($data as $k=>$v){
+            if ($v->$file_pid==$pid){
+                $data[$k]['_'.$file_name] = $data[$k][$file_name];
+                $arr[] = $data[$k];
+                foreach ($data as $m=>$n){
+                    if ($n->$file_pid == $v->$file_id){
+                        $data[$m]['_'.$file_name] = "┣━ ".$data[$m][$file_name];
+                        $arr[] = $data[$m];
+                    }
+                }
+            }
+        }
+        return $arr;
+    }
 }
